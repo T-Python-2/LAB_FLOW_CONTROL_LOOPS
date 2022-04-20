c298abd68826780d3ede5658ce9bf4f70e43d85a
Areejalharbi57 <areej.mohammad.ruh.s@tuwaiq.edu.sa>
2022-04-20 14:35:08 +0300

    update

--- lab3.py
+++ lab3.py
@@ -7,14 +7,17 @@ for n in range(45,210) :
     print(n)
 
 
-product =168
-answer = input("what is the product of 7 * 24 ?")
-if answer == product:
-    print("You answered this Question correctly")
+product = 168
+answer = int(input("what is the product of 7 * 24 ?"))
 
 while answer != product:
-    print("Your Answer is wrong try again..") 
-    answer = input("what is the product of 7 * 24 ?")
+    print("Your Answer is wrong try again..")
+    answer = int(input("what is the product of 7 * 24 ?"))
+    if answer == product:
+        print("You answered this Question correctly")
+
+
+ 
 
    
       
